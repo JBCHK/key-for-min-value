4e462957fcd0f7fc7da42b380eefb513d7e237b8
Jonathan Block <blockjonathan29@gmail.com>
2021-01-21 18:54:38 -0500

    commit tree 097dac82f4e43d8bda414ba1d7dd34dfb3c330e7

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  least_n = nil
+  least_i = nil
+  name_hash.each do |name, int| 
+    if  least_i == nil 
+        least_i = int
+        least_n = name
+  elsif least_i > int
+         least_i = int
+         least_n = name
+     end
+  end
+  least_n
 end
+  
+ 
